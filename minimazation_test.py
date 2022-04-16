54fdc9b4f0ac4b98aaaa692acbbd289bb344b56f
kbrsw <kbroz82@gmail.com>
2022-04-16 13:01:02 +0300

    new try to find solution for optimization using simple example

--- minimazation_test.py
+++ minimazation_test.py
@@ -2,21 +2,41 @@ from sympy import *
 import numpy as np
 import mpmath
 import math
-from scipy.optimize import minimize, basinhopping, leastsq
+from scipy.optimize import minimize, basinhopping, leastsq, fsolve, root
 
+#---Initial guess for A and B
+A = 2
+B = 1
 
-def func(x):
-    eq = abs(2*x[0]+3*x[1]-4000)
-    return eq
+#------find x and y at initial A and B
+def f1(x):
+    eq1 = A*x-5
+    return eq1
 
+def f2(y):
+    eq2 = B*y+6
+    return eq2
 
+xin = fsolve(f1, 1)
+print("xin = ", xin)
 
-x0 = np.array([3]*2)
-x1bound=(0,5000)
-x2bound=(0,6000)
+yin = fsolve(f2, 1)
+print("yin = ", yin)
 
+#-----Compare x-y calculated and measured
+diffCalc = xin-yin
+diffM = 5
+residue = diffCalc-diffM
+print("residue = ", residue)
 
+# ---- Optimizing X and Y takin into account conditions and given difference between
+def optfunc(x):
+    op1 = abs(xin[0]-yin[0]-x[0]+x[1])
+    op2 = abs(x[0]-x[1]-5)
+    return (op1,op2)
 
-opt = minimize(func, x0=x0, method='SLSQP', bounds=(x1bound, x2bound))
+optxy = root(optfunc,(xin[0],yin[0]))
+print(optxy)
 
-print(opt)
+diff= optxy.x[0]-optxy.x[1]
+print(diff)
