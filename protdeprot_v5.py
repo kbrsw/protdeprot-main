c0905fe9e78a65e9b9e70a8e11e3599b32c3b2d8
KostyaPC <kbroz82@gmail.com>
2022-04-18 13:18:33 +0300

    newest version of solver for protonation deprotonation constants

--- protdeprot_v5.py
+++ protdeprot_v5.py
@@ -1,7 +1,7 @@
 import mpmath
 import numpy as np
 from sympy import *
-from scipy.optimize import root, newton, basinhopping, minimize, fsolve
+from scipy.optimize import root, newton, basinhopping, minimize, fsolve, brute
 import sympy as sym
 import math as mt
 import scipy
@@ -31,7 +31,7 @@ def eq1(x):
     return ((x * h) / ((n - x) * M)) * np.exp(Aain * (x / n)) - Kain
 
 
-solution1 = fsolve(eq1, 0)
+solution1 = fsolve(eq1, n)
 print("som initial =", solution1)
 
 
@@ -39,7 +39,7 @@ def eq2(y):
     return ((y * oh) / ((n - y) * An)) * np.exp((Abin * y) / (n)) - Kbin
 
 
-solution2 = fsolve(eq2, 0)
+solution2 = fsolve(eq2, n)
 print("soh2x initial = ", solution2)
 
 # ---calculate sigmaCalc
@@ -51,52 +51,60 @@ print("residue = ", residue)
 
 i = 0
 pp = []
-while abs(residue) > 1e-12:
+while abs(residue) > 1e-19:
     # ------- Optimizing som and soh2x taking into account condition sigmaCalc = sigmaM
     def optfunc(x):
 
         op1 = (solution2[0] - solution1[0] - x[1] + x[0])**2
         op2 = (x[1] - x[0] - sigmaM)**2
-        return (op1+op2)
+        return (op1,op2)
 
     bound1 = (1e-12,1)
     bound2 = (1e-12,1)
-    optConc = minimize(optfunc, (solution1[0], solution2[0]), method = 'Powell', bounds=(bound1, bound2))
-    # print(optConc)
+    # optConc = minimize(optfunc, (solution1[0], solution2[0]), method = 'Powell', bounds=(bound1, bound2))
+    optConc = fsolve(optfunc, (solution1[0], solution2[0]))
 
-    residue1 = optConc.x[1] - optConc.x[0] - sigmaM
+
+    # residue1 = optConc.x[1] - optConc.x[0] - sigmaM
+    residue1 = optConc[1] - optConc[0] - sigmaM
     print("new residue = ", residue1)
+    print("optimized som ", optConc[0], "optimized soh2x ", optConc[1])
     pp.append(residue1)
     u = np.array(pp)
     residue = residue1
 
-
     # ------Estimate new Aa, Ka, Ab, Kb that agree with opimized som and soh2x
 
     def f1(z):
-        eqAA = (((optConc.x[0] * h) / ((n - optConc.x[0]) * M)) * np.exp(z[0] * (optConc.x[0] / n)) - z[1])**2
+        eqAA = abs(((optConc[0] * h) / ((n - optConc[0]) * M)) * np.exp(z[0] * (optConc[0] / n)) - z[1])
         # x[0]=Aa , x[1]=Ka
         return eqAA
 
 
     ### !!!!! --- Add boundaries
-    boundAa = (10, 300000)
-    boundKa = (1e-27, 1e-3)
-    optParA = minimize(f1, (Aain, Kain), method='Powell', bounds=(boundAa, boundKa))
+    boundAa = (0, 30000000000)
+    boundKa = (1e-38, 1)
+    optParA = minimize(f1, (Aain, Kain), method='Powell', bounds=(boundAa,boundKa))
+    # optParA = basinhopping(f1, (Aain, Kain))
+    # rranges = (slice(0, 1000,1), slice(0, 1000,1))
+    # optParA = brute(f1, (Aain, Kain), rranges, full_output=True)
     print("new Aa = ", optParA.x[0], "new Ka = ", optParA.x[1])
     Aain = optParA.x[0]
     Kain = optParA.x[1]
 
 
     def f2(y):
-        eqAB = (((optConc.x[1] * oh) / ((n - optConc.x[1]) * An)) * np.exp((y[0] * optConc.x[1]) / (n)) - y[1])**2
+        eqAB = abs(((optConc[1] * oh) / ((n - optConc[1]) * An)) * np.exp((y[0] * optConc[1]) / (n)) - y[1])
         # y[0] = Ab, y[1]=Kb
         return eqAB
 
 
-    boundAb = (10, 300000)
-    boundKb = (1e-27, 1e-3)
+    boundAb = (0, 30000000000)
+    boundKb = (1e-38, 1)
     optParB = minimize(f2, (Abin, Kbin), method='Powell', bounds=(boundAb, boundKb))
+    # optParB = basinhopping(f2, (Abin, Kbin))
+    # rranges=(slice(0,1000,1),slice(0,1000,1))
+    # optParB = brute(f2, (Abin, Kbin), rranges, full_output=True)
     print("new Ab = ", optParB.x[0], "new Kb = ", optParB.x[1])
 
     Abin = optParB.x[0]
@@ -104,8 +112,8 @@ while abs(residue) > 1e-12:
 
     print ("residue = ", residue)
 
-
-    solution1[0]=optConc.x[0]
-    solution2[0]=optConc.x[1]
+    n= optConc[0]
+    solution1[0]=optConc[0]
+    solution2[0]=optConc[1]
 
     i += 1
---
+++ test_brute.py
@@ -0,0 +1,12 @@
+from scipy.optimize import brute
+import numpy as np
+
+
+def f1(z):
+    x,y = z
+    return np.sin(2 / x + y)
+
+
+rranges = (slice(0, 10, 1), slice(0, 10, 1))
+optParA = brute(f1, rranges, full_output=True)
+print(" x = ", optParA[0], "y = ", optParA[1])
