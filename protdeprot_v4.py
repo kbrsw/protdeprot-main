7b33dfae34be3181757855980b5078193c4cc686
KostyaPC <kbroz82@gmail.com>
2022-04-18 11:37:28 +0300

    newest version of solver for protonation deprotonation constants

--- protdeprot_v4.py
+++ protdeprot_v4.py
@@ -6,85 +6,193 @@ import sympy as sym
 import math as mt
 import scipy
 
-
 sigmaM = float(input("Enter measured charge mineral charge, mol/m2 "))
 pH = float(input("Enter pH of solution "))
 M = float(input("Enter equilibrium concentration of metal in solution, mol/kg "))
 An = float(input("Enter concentration of anion in solution, mol/kg "))
 Ns = float(input("Enter a number of surface hydroxyl groups, mol/m2 "))
-h = 10**(-pH)
-oh = (1e-14)/h
-n = Ns/2
+h = 10 ** (-pH)
+oh = (1e-14) / h
+n = Ns / 2
 
 # Initial assumptions for constants
-Aain = 10**(1.1)
-Abin = 10**(3.8)
-Kain = 1e-4
-Kbin = 1.99e-10
+Aain = 0
+Abin = 0
+Kain = 0
+Kbin = 0
 
 
 def eq1(x):
-     return ((x*h)/((n-x)*M))*np.exp(Aain*(x/n)) - Kain
+    return ((x * h) / ((n - x) * M)) * np.exp(Aain * (x / n)) - Kain
+
+
 solution1 = fsolve(eq1, 0)
 print("Equation for Ka and Aa")
 print("som =", solution1)
 
+
 def eq2(y):
     return ((y * oh) / ((n - y) * An)) * np.exp((Abin * y) / (n)) - Kbin
+
+
 solution2 = fsolve(eq2, 0)
 print("Equation for Kb and Ab")
 print("soh2x = ", solution2)
 
-#---- Optimization of som and soh to obtain sigmaCalc = sigmaMeas
-z = np.array([0]*2)
-z[0] = solution1[0]
-z[1] = solution2[0]
+# ---- Optimization of som and soh to obtain sigmaCalc = sigmaMeas
 
 
-x0 = np.array([0]*2)
+# ---Initial guesses are solutions of equations for som and soh2x
+a = solution1[0]
+b = solution2[0]
 
-somBound = (1e-16,1)
-soh2xBound = (1e-16,1)
+x0 = np.array([a, b])
+
+somBound = (1e-09, Ns / 2)
+soh2xBound = (1e-09, Ns / 2)
+
 
 def residue(z):
-    f = abs(z[1] -z[0] - sigmaM)
+    f = abs(z[1] - z[0] - sigmaM)
     return f
 
-opt = minimize(residue, x0=x0, method='SLSQP', bounds=(somBound, soh2xBound))
+
+opt = minimize(residue, x0=x0, method="SLSQP", bounds=(somBound, soh2xBound))
 print("som optimized = ", opt.x[0])
 print("soh2x optimized = ", opt.x[1])
 
-#------Substitute optimized som and soh2x in equations for constants and optimizing Ka, Aa, Kb, Ab
+# ------Substitute optimized som and soh2x in equations for constants and optimizing Ka, Aa, Kb, Ab
+
 som = opt.x[0]
 soh2x = opt.x[1]
-#---Equation 1
-x = np.array([0]*2)
-x[0] = Aain
-x[1] = Kain
 
-#--boundaries Aa and Ka
-boundAa = (0,300)
-boundKa = (1e-10, 1e-1)
+# ---Equation 1
 
-x0 = np.array([Aain, Kain]) # Initial guess
+# --boundaries Aa and Ka
+boundAa = (10**1, 10**1.6)
+boundKa = (10**(-10.6), 10**(-3.98))
+
+x0 = np.array([Aain, Kain])  # Initial guess
+
 
 def eqopt1(x):
     return abs(((som * h) / ((n - som) * M)) * np.exp(x[0] * (som / n)) - x[1])
-optEq1 = minimize(eqopt1, x0=x0, method='SLSQP', bounds=(boundAa, boundKa))
+
+
+optEq1 = minimize(eqopt1, x0=x0, method="SLSQP", bounds=(boundAa, boundKa))
 print("Optimized Aa = ", optEq1.x[0], "Optimized Ka = ", optEq1.x[1])
 
-#---Equation 2
-y = np.array([0]*2)
-y[0] = Abin
-y[1] = Kbin
+# ---Equation 2
 
-#--boundaries Ab and Kb
-boundAb = (0,300)
-boundKb = (1e-20, 1e-5)
+# --boundaries Ab and Kb
+boundAb = (1, 10**3.9)
+boundKb = (10**(-27), 10**(-2))
 
-y0 = np.array([Abin, Kbin]) #Initial guess
+y0 = np.array([Abin, Kbin])  # Initial guess
+
 
 def eqopt2(y):
-    return ((soh2x * oh) / ((n - soh2x) * An)) * np.exp((y[0] * soh2x) / (n)) - y[1]
-optEq2 = minimize(eqopt2, x0=y0, method='SLSQP', bounds=(boundAb, boundKb))
+    return abs(((soh2x * oh) / ((n - soh2x) * An)) * np.exp((y[0] * soh2x) / (n)) - y[1])
+
+
+optEq2 = minimize(eqopt2, x0=y0, method="SLSQP", bounds=(boundAb, boundKb))
 print("Optimized Ab = ", optEq2.x[0], "Optimized Kb = ", optEq2.x[1])
+
+# ----------------------Take optimized Ka, Aa, Kb, Ab and solve equations again
+
+i = 0
+o = []
+while abs(opt.x[1] - opt.x[0] - sigmaM) > 1e-12:
+
+    Aain = optEq1.x[0]
+    Kain = optEq1.x[1]
+    GuessSOM = som
+
+
+    def eq1(x):
+        return ((x * h) / ((n - x) * M)) * np.exp(Aain * (x / n)) - Kain
+
+
+    solution1 = fsolve(eq1, GuessSOM)
+    # print("Equation for Ka and Aa")
+    # print("som =", solution1)
+
+    Abin = optEq2.x[0]
+    Kbin = optEq2.x[1]
+    GuessSOH2X = soh2x
+
+
+    def eq2(y):
+        return ((y * oh) / ((n - y) * An)) * np.exp((Abin * y) / (n)) - Kbin
+
+
+    solution2 = fsolve(eq2, GuessSOH2X)
+    # print("Equation for Kb and Ab")
+    # print("soh2x = ", solution2)
+
+
+    # ----Optimizing SOM and SOH2 to obtain SigmaCalc = SigmaM
+
+    a = solution1[0]
+    b = solution2[0]
+    x0 = np.array([a, b])
+
+    somBound = (1e-09, Ns / 2)
+    soh2xBound = (1e-09, Ns / 2)
+
+
+    def residue(z):
+        f = abs(z[1] - z[0] - sigmaM)
+        return f
+
+
+    opt = minimize(residue, x0=x0, bounds=(somBound, soh2xBound))
+    # print("som optimized = ", opt.x[0])
+    # print("soh2x optimized = ", opt.x[1])
+
+
+    # -----Opimization route for parameters 2
+    som = opt.x[0]
+    soh2x = opt.x[1]
+    # ---Equation 1
+    x = np.array([0] * 2)
+
+    # --boundaries Aa and Ka
+    boundAa = (10 ** 1, 10 ** 1.6)
+    boundKa = (10 ** (-10.6), 10 ** (-3.98))
+
+    x0 = np.array([Aain, Kain])  # Initial guess
+
+
+    def eqopt1(x):
+        return abs(((som * h) / ((n - som) * M)) * np.exp(x[0] * (som / n)) - x[1])
+    optEq1 = minimize(eqopt1, x0=x0, method="SLSQP", bounds=(boundAa, boundKa))
+
+
+    # ---Equation 2
+
+    # --boundaries Ab and Kb
+    boundAb = (1, 10 ** 3.9)
+    boundKb = (10 ** (-27), 10 ** (-2))
+
+    y0 = np.array([Abin, Kbin])  # Initial guess
+
+
+    def eqopt2(y):
+        return abs(((soh2x * oh) / ((n - soh2x) * An)) * np.exp((y[0] * soh2x) / (n)) - y[1])
+    optEq2 = minimize(eqopt2, x0=y0, method="SLSQP", bounds=(boundAb, boundKb))
+
+    print("residue = ", solution2[0] - solution1[0] - sigmaM)
+
+    res = soh2x - som - sigmaM
+    o.append(res)
+    array = np.array(o)
+
+    if abs(opt.x[1] - opt.x[0] - sigmaM) < 1e-12:
+        print("Aa = ", optEq1.x[0], "Ka = ", optEq1.x[1], "Ab = ", optEq2.x[0], "Kb = ", optEq2.x[1])
+
+    # if array[i] == array[i+1]:
+    #     print("Aa = ", optEq1.x[0], "Ka = ", optEq1.x[1], "Ab = ", optEq2.x[0], "Kb = ", optEq2.x[1])
+    #     break
+
+    i += 1
--- protdeprot_v5.py
+++ protdeprot_v5.py
@@ -6,183 +6,106 @@ import sympy as sym
 import math as mt
 import scipy
 
-
 sigmaM = float(input("Enter measured charge mineral charge, mol/m2 "))
 pH = float(input("Enter pH of solution "))
 M = float(input("Enter equilibrium concentration of metal in solution, mol/kg "))
 An = float(input("Enter concentration of anion in solution, mol/kg "))
 Ns = float(input("Enter a number of surface hydroxyl groups, mol/m2 "))
-h = 10**(-pH)
-oh = (1e-14)/h
-n = Ns/2
+h = 10 ** (-pH)
+oh = (1e-14) / h
+n = Ns / 2
 
 # Initial assumptions for constants
-Aain = 10**(1.1)
-Abin = 10**(3.8)
-Kain = 1e-4
-Kbin = 1.99e-10
+# Aain = 10 ** (1.1)
+# Abin = 10 ** (3.8)
+# Kain = 1e-4
+# Kbin = 1.99e-10
+Aain = 11
+Abin = 11
+Kain = 1e-6
+Kbin = 1e-6
 
 
+# -------find som and soh2x at initial Aa, Ka, Ab, Kb
 def eq1(x):
-     return ((x*h)/((n-x)*M))*np.exp(Aain*(x/n)) - Kain
+    return ((x * h) / ((n - x) * M)) * np.exp(Aain * (x / n)) - Kain
+
+
 solution1 = fsolve(eq1, 0)
-print("Equation for Ka and Aa")
-print("som =", solution1)
+print("som initial =", solution1)
+
 
 def eq2(y):
     return ((y * oh) / ((n - y) * An)) * np.exp((Abin * y) / (n)) - Kbin
+
+
 solution2 = fsolve(eq2, 0)
-print("Equation for Kb and Ab")
-print("soh2x = ", solution2)
+print("soh2x initial = ", solution2)
 
-x = np.array([0]*2)
-x[0] = solution1[0]
-x[1] = solution2[0]
-
-x0 = np.array([0]*2)
-
-somBound = (1e-16,1)
-soh2xBound = (1e-16,1)
-
-def residue(x):
-    f = abs(x[1] -x[0] - sigmaM)
-    return f
-
-# x0 = np.array([x[0], x[1])
-
-print("Residue ", residue(x))
-
-opt = minimize(residue, x0=x0, method='SLSQP', bounds=(somBound, soh2xBound))
-print("optimized som = ",opt.x[0])
-print("optimized soh2x = ",opt.x[1])
-print("optimized residue = ", opt.x[1]-opt.x[0]-sigmaM)
+# ---calculate sigmaCalc
+sigmaCalc = solution2 - solution1
+residue = sigmaCalc - sigmaM
+print("residue = ", residue)
 
 
-som = opt.x[0]
-soh2x = opt.x[1]
-#-----
-def min(x):
-    # x[0] = Aa
-    # x[1]=Ka
-    # x[2] = Ab
-    # x[3] = Kb
-    eqGen = abs(((som*h)/((n-som)*M))*np.exp(x[0]*(som/n)) - x[1] - ((soh2x * oh) / ((n - soh2x) * An)) * np.exp((x[2] * soh2x) / (n)) + x[3])
-    return eqGen
 
-x0 = np.array([10**(1.35), 1e-4, 10**(3.8), 1.99e-10])
+i = 0
+pp = []
+while abs(residue) > 1e-12:
+    # ------- Optimizing som and soh2x taking into account condition sigmaCalc = sigmaM
+    def optfunc(x):
 
-x1bond = (5,100)
-x2bond = (1e-13, 1e-6)
-x3bond = (10,10000)
-x4bond = (1e-18, 1e-6)
-opt1 = minimize(min, x0=x0, method='SLSQP', bounds=(x1bond, x2bond, x3bond, x4bond))
-print("Aa = ", opt1.x[0],"Ka = ", opt1.x[1], "Ab = ", opt1.x[2], "Kb = ", opt1.x[3])
+        op1 = (solution2[0] - solution1[0] - x[1] + x[0])**2
+        op2 = (x[1] - x[0] - sigmaM)**2
+        return (op1+op2)
 
-i=0
-while (opt.x[1]-opt.x[0]-sigmaM) > 1e-12:
+    bound1 = (1e-12,1)
+    bound2 = (1e-12,1)
+    optConc = minimize(optfunc, (solution1[0], solution2[0]), method = 'Powell', bounds=(bound1, bound2))
+    # print(optConc)
 
-    # x = opt.x[0]
-    # y = opt.x[1]
-
-    def eq1(x):
-        return ((x * h) / ((n - x) * M)) * np.exp(opt1.x[0] * (x / n)) - opt1.x[1]
-    solution1 = fsolve(eq1, 0)
+    residue1 = optConc.x[1] - optConc.x[0] - sigmaM
+    print("new residue = ", residue1)
+    pp.append(residue1)
+    u = np.array(pp)
+    residue = residue1
 
 
-    def eq2(y):
-        return ((y * oh) / ((n - y) * An)) * np.exp((opt1.x[2] * y) / (n)) - opt1.x[3]
-    solution2 = fsolve(eq2, 0)
+    # ------Estimate new Aa, Ka, Ab, Kb that agree with opimized som and soh2x
 
-    solution1[0] = som
-    solution2[0] = soh2x
-
-    som = x[0]
-    soh2x = x[1]
-
-    def residue(x):
-        f = abs(x[1] - x[0] - sigmaM)
-        return f
-
-        
-
-    print("Residue ", residue(x))
-
-    somBound = (1e-16, 1)
-    soh2xBound = (1e-16, 1)
-
-    opt = minimize(residue, x0=x0, method='SLSQP', bounds=(somBound, soh2xBound))
-    print("optimized residue = ", opt.x[1] - opt.x[0] - sigmaM)
-
-    # -----
-    def min(x):
-        # x[0] = Aa
-        # x[1]=Ka
-        # x[2] = Ab
-        # x[3] = Kb
-        eqGen = abs(((som * h) / ((n - som) * M)) * np.exp(x[0] * (som / n)) - x[1] - (
-                    (soh2x * oh) / ((n - soh2x) * An)) * np.exp((x[2] * soh2x) / (n)) + x[3])
-        return eqGen
+    def f1(z):
+        eqAA = (((optConc.x[0] * h) / ((n - optConc.x[0]) * M)) * np.exp(z[0] * (optConc.x[0] / n)) - z[1])**2
+        # x[0]=Aa , x[1]=Ka
+        return eqAA
 
 
-    x0 = np.array([opt1.x[0], opt1.x[1], opt1.x[2], opt1.x[3]])
+    ### !!!!! --- Add boundaries
+    boundAa = (10, 300000)
+    boundKa = (1e-27, 1e-3)
+    optParA = minimize(f1, (Aain, Kain), method='Powell', bounds=(boundAa, boundKa))
+    print("new Aa = ", optParA.x[0], "new Ka = ", optParA.x[1])
+    Aain = optParA.x[0]
+    Kain = optParA.x[1]
 
-    x1bond = (5, 100)
-    x2bond = (1e-13, 1e-6)
-    x3bond = (10, 10000)
-    x4bond = (1e-18, 1e-6)
-    opt1 = minimize(min, x0=x0, method='SLSQP', bounds=(x1bond, x2bond, x3bond, x4bond))
-    print("Aa = ", opt1.x[0], "Ka = ", opt1.x[1], "Ab = ", opt1.x[2], "Kb = ", opt1.x[3])
 
-    i+=1
+    def f2(y):
+        eqAB = (((optConc.x[1] * oh) / ((n - optConc.x[1]) * An)) * np.exp((y[0] * optConc.x[1]) / (n)) - y[1])**2
+        # y[0] = Ab, y[1]=Kb
+        return eqAB
 
-# Ka = minimize(residue, [0], method= 'SLSQP', args=(Kain))
-# print("optimized Ka", Ka.x[0])
-#
-# Kb = minimize(residue, [0], method= 'SLSQP', args=(Kbin))
-# print("optimized Kb", Kb.x[0])
-#
-# Aa = minimize(residue, [0], method= 'SLSQP', args=(Aain))
-# print("optimized Aa", Aa.x[0])
-#
-# Ab = minimize(residue, [0], method= 'SLSQP', args=(Abin))
-#
-# print("optimized Ab", Ab.x[0])
-#
-# o = []
-#
-# while soh2x-soh2x-sigmaM > 1e-8:
-#
-#     v = np.array(o)
-#
-#     Ka = minimize(residue, [0], method= 'Powell', args=(Ka.x[0]))
-#
-#
-#     Kb = minimize(residue, [0], method= 'Powell', args=(Kb.x[0]))
-#
-#
-#     Aa = minimize(residue, [0], method= 'Powell', args=(Aa.x[0]))
-#
-#
-#     Ab = minimize(residue, [0], method= 'Powell', args=(Ab.x[0]))
-#
-#
-#     def eq1(x):
-#         return ((x*h)/((n-x)*M))*np.exp(Aa.x[0]*(x/n)) - Ka.x[0]
-#     solution1 = fsolve(eq1, 0)
-#
-#     def eq2(y):
-#         return ((y * oh) / ((n - y) * An)) * np.exp((Ab.x[0] * y) / (n)) - Kb.x[0]
-#     solution2 = fsolve(eq2, 0)
-#
-#
-#     som = solution1[0]
-#     soh2x=solution2[0]
-#
-#     sigmaCalc = soh2x-som
-#
-#     o.append(sigmaCalc)
-#
-#     print("new residue ", sigmaCalc-sigmaM)
-#
-#     i+=1
 
+    boundAb = (10, 300000)
+    boundKb = (1e-27, 1e-3)
+    optParB = minimize(f2, (Abin, Kbin), method='Powell', bounds=(boundAb, boundKb))
+    print("new Ab = ", optParB.x[0], "new Kb = ", optParB.x[1])
+
+    Abin = optParB.x[0]
+    Kbin = optParB.x[1]
+
+    print ("residue = ", residue)
+
+
+    solution1[0]=optConc.x[0]
+    solution2[0]=optConc.x[1]
+
+    i += 1
