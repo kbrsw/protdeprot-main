b0f19036e1fb90bce1cf562ab8d3340ef20b7039
KostyaPC <kbroz82@gmail.com>
2022-04-20 16:47:27 +0300

    20042022

--- protdeprot_v5.py
+++ protdeprot_v5.py
@@ -1,7 +1,7 @@
 import mpmath
 import numpy as np
 from sympy import *
-from scipy.optimize import root, newton, basinhopping, minimize, fsolve, brute
+from scipy.optimize import root, newton, basinhopping, minimize, fsolve, brute, least_squares
 import sympy as sym
 import math as mt
 import scipy
@@ -26,63 +26,77 @@ Kain = 1e-6
 Kbin = 1e-15
 
 
-i = 0
+
 residue = 1
 x0 = (11,1e-6, 11, 1e-6)
 pp1 = []
 pp2 = []
+pp3 = []
+pp4 = []
 
 # -------find som and soh2x at initial Aa, Ka, Ab, Kb
 def eq1(x):
     return ((x * h) / ((n - x) * M)) * np.exp(Aain * (x / n)) - Kain
 
 
-solution1 = fsolve(eq1, n)
-print("som initial =", solution1)
+# ----Derivation
+x = Symbol('x')
+f1 = ((x * h) / ((n - x) * M)) * exp(Aain * (x / n)) - Kain
+
+differ1 = f1.diff(x)
+# --- Convert from sympy to numpy expression
+deriveq1 = lambdify([x], [differ1], "numpy")
+
+solution1 = newton(eq1, 0, deriveq1)
+print(solution1[0])
 
 
 def eq2(y):
     return ((y * oh) / ((n - y) * An)) * np.exp((Abin * y) / (n)) - Kbin
 
 
-solution2 = fsolve(eq2, n)
-print("soh2x initial = ", solution2)
+y = Symbol('y')
+f2 = ((y * oh) / ((n - y) * An)) * exp((Abin * y) / n) - Kbin
 
-# ---calculate sigmaCalc
-sigmaCalc = solution2 - solution1
-residue = sigmaCalc - sigmaM
-print("residue = ", residue)
+differ2 = f2.diff(y)
+deriveq2 = lambdify([y], [differ2], "numpy")
+solution2 = newton(eq2, 0, deriveq2)
 
+print(solution2[0])
+i = 0
 while abs(residue) > 1e-12:
-    # ------- Optimizing som and soh2x taking into account condition sigmaCalc = sigmaM
-    def optfunc(x):
 
-        op1 = abs(solution2[0] - solution1[0] - x[1] + x[0])
-        op2 = (x[1] - x[0] - sigmaM)
+    # ---calculate sigmaCalc
+    sigmaCalc = solution2 - solution1
+    residue = sigmaCalc - sigmaM
+    print("residue = ", residue[0])
+
+    ss1 = solution1[0]
+    ss2 = solution2[0]
+
+    # ------- Optimizing som and soh2x taking into account condition sigmaCalc = sigmaM
+    def optfunc(z):
+        # x[0] = x
+        # x[1] = y
+        x,y = z
+        op1 = abs(ss2 - ss1 - y + x)
+        op2 = (y - x - sigmaM)
         return (op1,op2)
 
     bound1 = (1e-12,1)
     bound2 = (1e-12,1)
-    # optConc = minimize(optfunc, (solution1[0], solution2[0]), method = 'Powell', bounds=(bound1, bound2))
-    optConc = fsolve(optfunc, (solution1[0], solution2[0]))
+    optConc = least_squares(optfunc, (ss1, ss2), bounds = ((1e-16, 1e-16),(1,1)))
 
 
+    residue = optConc.x[1] - optConc.x[0] - sigmaM
 
-    # residue1 = optConc.x[1] - optConc.x[0] - sigmaM
-    residue = optConc[1] - optConc[0] - sigmaM
-    pp1.append(optConc[1])
-    pp2.append(optConc[0])
     print("new residue = ", residue)
-    print("optimized som ", optConc[0], "optimized soh2x ", optConc[1])
-
-
-    # residue = residue1
-
+    print("optimized som ", optConc.x[0], "optimized soh2x ", optConc.x[1])
 
     # ------Estimate new Aa, Ka, Ab, Kb that agree with opimized som and soh2x
 
     def f1(z):
-        eqAA = abs((((optConc[0] * h) / ((n - optConc[0]) * M)) * np.exp(z[0] * (optConc[0] / n)) - z[1])+(((optConc[1] * oh) / ((n - optConc[1]) * An)) * np.exp((z[2] * optConc[1]) / (n)) - z[3]))
+        eqAA = abs((((optConc.x[0] * h) / ((n - optConc.x[0]) * M)) * np.exp(z[0] * (optConc.x[0] / n)) - z[1])+(((optConc.x[1] * oh) / ((n - optConc.x[1]) * An)) * np.exp((z[2] * optConc.x[1]) / (n)) - z[3]))
         # x[0]=Aa , x[1]=Ka
         return eqAA
 
@@ -104,17 +118,20 @@ while abs(residue) > 1e-12:
     Abin = optParA.x[2]
     Kbin = optParA.x[3]
 
-    u = np.array(pp1)
-    v = np.array(pp2)
+    n = optConc.x[0]
+    solution1[0] = optConc.x[0]
+    solution2[0] = optConc.x[1]
 
-    n = optConc[0]
-    solution1[0]=optConc[0]
-    solution2[0]=optConc[1]
-
+    pp1.append(Aain)
+    pp2.append(Kain)
+    pp3.append(Abin)
+    pp4.append(Kbin)
 
     i += 1
 
-
-
+    u = np.array(pp1)
+    v = np.array(pp2)
+    e = np.array(pp3)
+    w = np.array(pp4)
 
 
--- test_newtonraphson.py
+++ test_newtonraphson.py
@@ -1,13 +1,47 @@
 from scipy.optimize import newton
 import numpy as np
+from sympy import *
 
 
-def f1(z):
-    x,y = z
-    return np.sin(2 / x + y)
+sigmaM = float(input("Enter measured charge mineral charge, mol/m2 "))
+pH = float(input("Enter pH of solution "))
+M = float(input("Enter equilibrium concentration of metal in solution, mol/kg "))
+An = float(input("Enter concentration of anion in solution, mol/kg "))
+Ns = float(input("Enter a number of surface hydroxyl groups, mol/m2 "))
+h = 10 ** (-pH)
+oh = (1e-14) / h
+n = Ns / 2
 
-xr = (1,1000)
-yr = (1,1000)
+Aain = 11
+Abin = 11
+Kain = 1e-6
+Kbin = 1e-6
 
-optParA = minimize(f1, (100,100), bounds=(xr,yr))
-print(" x = ", optParA.x[0], "y = ", optParA.x[1])
+
+def eq1(x):
+     return ((x * h) / ((n - x) * M)) * np.exp(Aain * (x / n)) - Kain
+
+
+#----Derivation
+x = Symbol('x')
+f1=((x * h) / ((n - x) * M)) * exp(Aain * (x / n)) - Kain
+
+differ1 = f1.diff(x)
+#--- Convert from sympy to numpy expression
+deriveq1 = lambdify([x], [differ1], "numpy")
+
+
+solution1 = newton(eq1, 0, deriveq1)
+print(solution1[0])
+
+def eq2(y):
+    return ((y * oh) / ((n - y) * An)) * np.exp((Abin * y) / (n)) - Kbin
+
+y = Symbol('y')
+f2 = ((y * oh) / ((n - y) * An)) * exp((Abin * y) / n) - Kbin
+
+differ2 = f2.diff(y)
+deriveq2 = lambdify([y], [differ2], "numpy")
+solution2 = newton(eq2, 0, deriveq2)
+
+print(solution2[0])
