544a5f0ecd99cf63bc092c52631c2855369d4694
KostyaPC <kbroz82@gmail.com>
2022-04-21 17:10:46 +0300

    Minimization tests

--- test_minimization.py
+++ test_minimization.py
@@ -1,13 +1,16 @@
 from scipy.optimize import minimize
 
-def function(z):
-    x,y = z
-    eq = ((150*x+137*y)*28-5000)**2
+def function(a):
+    x,y,z = a
+    eq = abs(370*8+((150*x+90*y+110*z)*8)-8900)
     return eq
 
-boundx = (0,10)
-boundy = (0,10)
+boundx = (0,3)
+boundy = (0,3)
+boundz = (0,3)
+
 # opt = minimize(function, (0,0), method = 'SLSQP', bounds=(boundx, boundy))
-opt = minimize(function, (0,0), bounds=(boundx, boundy))
+opt = minimize(function, (0,0,0), bounds=(boundx, boundy, boundz))
 print("x = ", opt.x[0])
 print("y = ", opt.x[1])
+print("z = ", opt.x[2])
